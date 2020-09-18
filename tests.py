1a3766f78041df7fe21e5cd9fecab3e8bf2da566
Chris Richardson <cnr12@cam.ac.uk>
2020-09-18 08:40:49 +0100

    Remove from tests

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 
-from maclaurin import Sin, Exp, Log1
+from maclaurin import Sin
 import math
 
 
@@ -8,11 +8,4 @@ def test_sin():
     assert abs(w.evaluate(10, 0.5) - math.sin(0.5)) < 1e-10
 
 
-def test_exp():
-    w = Exp()
-    assert abs(w.evaluate(20, 0.5) - math.exp(0.5)) < 1e-10
 
-
-def test_log1():
-    w = Log1()
-    assert abs(w.evaluate(50, 0.5) - math.log(1.5)) < 1e-10
